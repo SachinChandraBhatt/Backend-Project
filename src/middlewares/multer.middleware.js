414a21fd11eef29748240dbc4592b3c6d86171f8
SachinChandraBhatt <ms20051502@gmail.com>
2024-12-30 18:54:25 +0530

    add clodinary and multer functionality

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,7 +1,15 @@
-import multer from "multer"
+import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination:function (req , file) {
-        
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    // should do change here by yourself
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname);
+  },
+});
+
+// const upload = multer({ storage: storage });
+const upload = multer({ storage });
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,39 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import ApiError from "./ApiError.js"
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+const uploadOnCloudinery = async (localFilePath)=>{
+    try {
+        if (!localFilePath) {
+            return null;
+        }
+        const response = await cloudinary.uploader.upload(localFilePath , {
+            resource_type:"auto"
+        })
+        //file has been uploaded successfully
+        console.log("file is uploaded in cloudinary" , response.url());
+        return response;        
+    } catch (error) {
+        // remove the locally saved temporary file
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+// const uploadResult = await cloudinary.uploader
+//        .upload(
+//            'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+//                public_id: 'shoes',
+//            }
+//        )
+//        .catch((error) => {
+//            console.log(error);
+//        });
+
